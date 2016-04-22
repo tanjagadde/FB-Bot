66fed8f5fa6ff027177d438baa3ef89a6944607a
Gadde, Tanuja <Tanuja.Gadde@pearson.com>
2016-04-22 11:55:10 -0600

    change in app.post update access token

--- polis.js
+++ polis.js
@@ -37,7 +37,7 @@ app.get('/webhook', function (req, res) {
   }
 });
 
-app.post('/webhook/', function (req, res) {
+app.post('/', function (req, res) {
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
     event = req.body.entry[0].messaging[i];

f072761c39032f6940be2980505e9d1c0ab31d9a
Gadde, Tanuja <Tanuja.Gadde@pearson.com>
2016-04-22 17:56:01 -0600

    major changes

--- polis.js
+++ polis.js
@@ -49,16 +49,16 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res) {
   console.log("request is "+req.body)
-  messaging_events = req.body.entry[0].messaging;
-  for (i = 0; i < messaging_events.length; i++) {
-    event = req.body.entry[0].messaging[i];
-    sender = event.sender.id;
-    if (event.message && event.message.text) {
-      text = event.message.text;
-      // Handle a text message from this sender
-      sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
-    }
-  }
+  // messaging_events = req.body.entry[0].messaging;
+  // for (i = 0; i < messaging_events.length; i++) {
+  //   event = req.body.entry[0].messaging[i];
+  //   sender = event.sender.id;
+  //   if (event.message && event.message.text) {
+  //     text = event.message.text;
+  //     // Handle a text message from this sender
+  //     sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+  //   }
+  // }
   res.sendStatus(200);
 });
 
